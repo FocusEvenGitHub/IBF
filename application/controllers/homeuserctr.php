b3fac5ef36fa617604f33291603f0ba831ed728b
Henry Sampaio <henrykuntm@gmail.com>
2023-11-22 19:57:45 -0300

    [refs:#13]

--- application/controllers/homeuserctr.php
+++ application/controllers/homeuserctr.php
@@ -40,5 +40,19 @@ class homeuserctr extends CI_Controller {
 		$view["title"] = "Ministérios";
         $this->load->view('home/aside', $view);
         $this->load->view('home_user/ministerios', $view);
-    }
+ 	}
+	public function config(){
+		$view['scripts_header'][] = base_url('assets/plugins/jQuery/jquery-3.7.1.js'); 
+		$view['scripts_header'][] = base_url('assets/plugins/notify/notify.min.js'); // or notify.js
+        $view['scripts_header'][] = base_url('assets/js/home_user/ministerios.js');
+
+		$view['links_header'][] = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css';
+		$view['links_header'][] = base_url ('assets/css/main/font.css' );
+		$view['links_header'][] = base_url ('assets/css/main/home.css' );
+		$view['links_header'][] = base_url ('assets/css/user/homeUser.css' );
+
+		$view["title"] = "Ministérios";
+        $this->load->view('home/aside', $view);
+        $this->load->view('home_user/config/config_user', $view);
+	}
 }
--- assets/js/home_user/home_user.js
+++ assets/js/home_user/home_user.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
     // $.notify("test");
     $('#nav_home').addClass('active');
-
 });
